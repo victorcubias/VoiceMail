b2c1c8910552fcaf5b1387ac2c82b8901a8653a6
Víctor Fernando Cubías Cortez <victor.cubias@gmail.com>
2020-06-06 03:08:33 -0600

    Se retiran cosas innecesarias.

--- src/components/VoiceMail/index.js
+++ src/components/VoiceMail/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import ReactTable from "react-table";
 
 
-const serverUrl = 'https://sandbox.2600hz.com:8443/v2';
+
+const serverUrl = '/v2';
 const credentials = 'NDY0MmU2NDA0MGNkYjhiODljMzEwYTIxYTA3YzdmNjI6MjMyNjQxNTY1OTA3NWU3NTAwMGNlY2Q3YmNiZjM3NTY=';
 const accountId = '4642e64040cdb8b89c310a21a07c7f62';
 const vmBoxId = 'b37675a2d7b90d60f0ee5d4175502394';
@@ -13,36 +15,54 @@ const headers = {
 
 
 
+   
+
+
 class VoiceMail extends Component {
     constructor() {
-        super();
+        super();        
+        this.state = {
+            data : []  
+        }
     }
 
-    
-        handleUpdateClick = () => {
-            console.log(headers);
-            fetch(`${serverUrl}/accounts/${accountId}/vmboxes/${vmBoxId}/messages?paginate=true`,{ headers}).
-        then(function(response) {
-            console.log(response.json());
+async componentDidMount(){
+    await fetch(`${serverUrl}/accounts/${accountId}/vmboxes/${vmBoxId}/messages?paginate=true`,{ headers}).
+        then(function(response) {            
             if(!response.ok){                
                 throw Error(response.statusText);
             }
-            return response;
-        }).then(function(response){
-            console.log(response);
-            console.log("ok");
-        }).catch(function(error){
-            console.log(error);
-        });                                
-            }                                
-     
+            return response.json();
+        }).then((response) =>{            
+            this.setState({                
+                data: response.data
+              });
+              console.log([response.data]);
+              this.render();   
+        });
+}
 
-    render() {
-        return(
-            <div>                                
-                <button onClick={this.handleUpdateClick}>Obtener Datos</button>                
+                                                 
+    render() {        
+        console.log("this.state.data");
+        console.log(this.state.data.data);
+        return (
+            <div>
+              {
+                this.state.data.map((dato,index) => {                                    
+                    return (
+                        <div key={index} style={{borderBottom: '2px solid black'}}>
+                          <p>Status: {dato.folder}</p>                                                                        
+                          <p>From: {dato.from}</p>                                                                        
+                          <p>To: {dato.to}</p>                                                                        
+                          <p>Duration: {dato.timestamp}</p>                                                                        
+                        </div>
+                      );
+                    })
+                
+              }
             </div>
-        );
+          )                                
     }
 
     
